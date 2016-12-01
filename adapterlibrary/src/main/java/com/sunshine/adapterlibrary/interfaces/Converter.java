a6cdc900efa5853bb9e80c1dbeb75b4eb6e0c2b4
gengqiquan <542400966@qq.com>
2016-12-01 16:12:46 +0800

    java8

--- adapterlibrary/src/main/java/com/sunshine/adapterlibrary/interfaces/Converter.java
+++ adapterlibrary/src/main/java/com/sunshine/adapterlibrary/interfaces/Converter.java
@@ -4,6 +4,7 @@ package com.sunshine.adapterlibrary.interfaces;
 /**
  * Created by 耿 on 2016/8/11.
  */
+@FunctionalInterface
 public interface Converter<T> {
     void convert(Holder holder, T item);
 
